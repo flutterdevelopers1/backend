500bff364e6b27d445c61333cdc4517b76f837b7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-21 20:44:15 +0530

    udpate mongodb url

--- src/private.rs
+++ src/private.rs
@@ -1,3 +1,3 @@
 pub const JWT_SECRET: &'static str = "MySecretKey";
 pub const REFRESH_JWT_SECRET: &'static str = "MySecretKey";
-pub(crate) const URL_DB: &str = "mongodb+srv://ghostdetectorteam:mytestpassword@clustter.3ehhyhw.mongodb.net/?retryWrites=true&w=majority/backend";
+pub(crate) const URL_DB: &str = "mongodb+srv://ghostdetectorteam:mytestpassword@clustter.3ehhyhw.mongodb.net/?retryWrites=true&w=majority";
